e3fdb73830302f599371f1e2190611e451940fef
Nicola Toscan <nicolatoscan99@outlook.it>
2019-02-28 20:11:14 +0100

    vert hor

--- HashCode-2019/Testing.cs
+++ HashCode-2019/Testing.cs
@@ -18,6 +18,8 @@ namespace HashCode_2019
 
 
         static public void CountFotoTag(List<Picture> pp) {
+            var vert = pp.Select(p => p.orientation == EOrientation.Horizontal).ToList();
+            var hor = pp.Select(p => p.orientation == EOrientation.Vertical).ToList();
             var gg = pp.Select(p => p.Tags.Count()).GroupBy(n => n).Select(n => (n.Key, n.Count())).ToList().OrderByDescending(jj => jj.Key);
             int hh = 0;
         }
@@ -26,7 +28,7 @@ namespace HashCode_2019
 
 
         static public BestMatch(List<Picture> listPicture, Picture picture) {
-            // var listMatch = listPicture.Select(p => p.Tags).Select(p)
+            var listMatch = listPicture.Select(p => p.Tags).Select(p)
         }
     }
 }
