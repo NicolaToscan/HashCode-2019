f6f6afa2369079af53f9dfa50723bbe279436866
Nicola Toscan <nicolatoscan99@outlook.it>
2019-02-28 20:17:46 +0100

    a

--- HashCode-2019/Testing.cs
+++ HashCode-2019/Testing.cs
@@ -8,6 +8,8 @@ namespace HashCode_2019
 {
     static public class Testing
     {
+        static List<Picture> hor;
+
         static public void CountTag(List<Picture> pp) {
             var a = pp.SelectMany(p => p.Tags).GroupBy(p => p).Select(p => (p.Key, p.Count())).OrderByDescending(t => t.Item2).ToList();
             var top = a[0];
@@ -18,17 +20,30 @@ namespace HashCode_2019
 
 
         static public void CountFotoTag(List<Picture> pp) {
-            var vert = pp.Select(p => p.orientation == EOrientation.Horizontal).ToList();
-            var hor = pp.Select(p => p.orientation == EOrientation.Vertical).ToList();
-            var gg = pp.Select(p => p.Tags.Count()).GroupBy(n => n).Select(n => (n.Key, n.Count())).ToList().OrderByDescending(jj => jj.Key);
+            var gg = pp.Select(p => p.Tags.Count()).GroupBy(n => n).Select(n => (n.Key, n.Count())).ToList().OrderByDescending(jj => jj.Item2);
             int hh = 0;
         }
 
 
 
 
-        static public BestMatch(List<Picture> listPicture, Picture picture) {
-            var listMatch = listPicture.Select(p => p.Tags).Select(p)
+        static public void BestMatch(List<Picture> listPicture, Picture picture) {
+            var listMatch = listPicture.Select(p => p.Tags).Select(tags => {
+                int common = picture.Tags.Intersect(tags).Count();
+                int diff = picture.Tags.Count() - common;
+                return 0;
+
+            });
         }
+
+        static public void SetHorizontal(List<Picture> pp) {
+            hor = pp.Where(p => p.orientation == EOrientation.Vertical).ToList();
+        }
+
+        static public void BestCompanionPicture(List<Picture> listPicture, Picture picture) {
+
+        }
+
+
     }
 }
